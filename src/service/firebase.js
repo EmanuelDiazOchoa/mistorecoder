1b6958c0535a1ba602371c9cfa03f15437451063
emanuel diaz ochoa <emagnr93@gmail.com>
2025-06-05 23:25:58 -0300

    Feat: finalizado sujeto a mejoras 2

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -6,7 +6,7 @@ import {
 import { getDatabase } from 'firebase/database';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// ✅ Configuración de tu proyecto
+
 const firebaseConfig = {
   apiKey: "AIzaSyCt-HbGqJeWDNJjJ0XKc5qppkrcbBHpHUY",
   authDomain: "ecomersmovil.firebaseapp.com",
@@ -17,16 +17,16 @@ const firebaseConfig = {
   appId: "1:392409110606:web:692dd3035725af10ba0b1f",
 };
 
-// ✅ Inicializamos solo si no fue inicializado antes
+
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-// ✅ Inicializamos auth con AsyncStorage para persistencia real
+
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
-// ✅ Base de datos Realtime Database
+
 const db = getDatabase(app);
 
-// ✅ Exportamos todo
+
 export { app, auth, db };
